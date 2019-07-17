e3e2e6f58920df83c6d2591fdaecc462d6a016d4
Billy Mathison <wlmathison@gmail.com>
2019-07-17 15:03:16 -0500

    Creating ProductCreateViewModel with list of product types and select list options

---
+++ Bangazon/Models/ProductViewModels/ProductCreateViewModel.cs
@@ -0,0 +1,19 @@
+﻿/* Author: Billy Mathison
+ * Purpose: Creating a view model for products including product, a list of product types, and a list of select list items for ProductType dropdown.
+ * */
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.ProductViewModels
+{
+    public class ProductCreateViewModel
+    {
+        public Product Product { get; set; }
+        public List<ProductType> ProductTypes { get; set; }
+        public List<SelectListItem> ProductTypeOptions => ProductTypes?.Select(p => new SelectListItem(p.Label, p.ProductTypeId.ToString())).ToList();
+    }
+}
