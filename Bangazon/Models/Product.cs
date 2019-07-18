9561dc8edf6953fb635ad1514463ea225090b5c2
William Mitchell <wemitchell3@gmail.com>
2019-07-18 17:38:58 -0500

    Commiting completed max product price and no special characters in product description and title.

--- Bangazon/Models/Product.cs
+++ Bangazon/Models/Product.cs
@@ -21,11 +21,15 @@ namespace Bangazon.Models
         public string Description { get; set; }
 
         [Required]
+        // Billy Mitchell: This Regular Expression is used to forbid users from usings these !@#$%^&*() characters. Use allow: [characters in here] or not allow [^characters in here]
+        [RegularExpression(@"^[^!@#$%^&*()]+$", ErrorMessage = "The characters !@#$%^&*() are not allowed")]
         [StringLength(55, ErrorMessage = "Please shorten the product title to 55 characters")]
         public string Title { get; set; }
 
         [Required]
         [DisplayFormat(DataFormatString = "{0:C}")]
+        //Billy Mitchell: This Data Annotation forbids the user from entering a price higher then 10,000
+        [Range(0.0, 10000.0, ErrorMessage = "Price cannot exceed $10,000")]
         public double Price { get; set; }
 
         [Required]
